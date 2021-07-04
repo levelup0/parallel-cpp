221632636e5e1d55e973793423f4223482b9ecfb
levelup0 <leo.rybakov@icloud.com>
2021-07-04 23:20:46 +0300

    Task 3 v0.1

--- deadlock/deadlock.h
+++ deadlock/deadlock.h
@@ -1,20 +1,33 @@
 #pragma once
+#include <mutex>
+#include <thread>
 
+using namespace std::chrono_literals;
 
 class Deadlock {
  public:
   Deadlock() {
+    x = y = 0;
   }
 
   void ThreadOne() {
-    // Your
+    x_mutex.lock();
+    std::this_thread::sleep_for(10ms);
+    y_mutex.lock();
+    x_mutex.unlock();
+    y_mutex.unlock();
   }
 
   void ThreadTwo() {
-    // Your
+    y_mutex.lock();
+    std::this_thread::sleep_for(10ms);
+    x_mutex.lock();
+    y_mutex.unlock();
+    x_mutex.unlock();
   }
 
  private:
-  // Your
+  std::mutex x_mutex, y_mutex;
+  int x, y;
 };
 
