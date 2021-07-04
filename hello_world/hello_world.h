1d354fc79cb192926fea27f4c000a5e415a98c16
levelup0 <leo.rybakov@icloud.com>
2021-07-04 23:11:43 +0300

    Task 2 v0.2

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -2,22 +2,34 @@
 
 #include <ostream>
 #include <string_view>
-
+#include <vector>
+#include <thread> 
+#include <iostream>
+#include <mutex>
 
 class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t /*n_threads*/)
-  {
-    // Your code
-  }
+  HelloWorld(size_t n_threads) : mN_threads(n_threads) {}
 
-  void SayHello(std::ostream& /*os*/) {
-    // Your code
+  void SayHello(std::ostream& out) {
+      std::vector<std::thread> threads;
+      std::mutex m;
+      for (size_t i = 0; i < mN_threads; ++i) {
+          threads.emplace_back([&] {
+                  m.lock();
+                  out << kHelloPrefix << std::this_thread::get_id() << '\n';
+                  m.unlock();
+                  });
+      }
+
+      for (auto &t : threads) {
+          t.join();
+      }
   }
 
  private:
-  // Your code
+  size_t mN_threads;
 };
 
