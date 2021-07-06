b76cea30f715b4b871a4b0747ebe7efe7fdb9672
levelup0 <leo.rybakov@icloud.com>
2021-07-06 15:38:08 +0300

    Task 6 v0.1

--- thread_safe_queue/thread_safe_queue.h
+++ thread_safe_queue/thread_safe_queue.h
@@ -1,40 +1,43 @@
-#pragma once
-
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
 #include <optional>
 #include <queue>
 
-
 template <typename T>
 class ThreadSafeQueue {
- public:
-  ThreadSafeQueue() {
-  }
-
-  void Push(const T& value) {
-    // Your code
-    queue_.push(value);
-  }
-
-  T Pop() {
-    // Your code
-    auto value = queue_.front();
-    queue_.pop();
-    return value;
-  }
-
-  std::optional<T> TryPop() {
-    // Your code
-    if (queue_.empty()) {
-      return std::nullopt;
+public:
+    ThreadSafeQueue() {
     }
-    auto value = queue_.front();
-    queue_.pop();
-    return value;
-  }
+
+    void Push(const T& value) {
+        std::unique_lock<std::mutex> lock(m_);
+        q_.push(value);
+        cv_.notify_one();
+    }
+
+    T Pop() {
+        std::unique_lock<std::mutex> lock(m_);
+        cv_.wait(lock, [&]{return !q_.empty();});
+        auto temp = q_.front();
+        q_.pop();
+        return temp;
+    }
+
+    std::optional<T> TryPop() {
+        std::unique_lock<std::mutex> lock(m_);
+        if (q_.empty()) {
+            return std::nullopt;
+        }
+        auto temp = q_.front();
+        q_.pop();
+
+        return temp;
+    }
 
 
- private:
-  // Your code
-  std::queue<T> queue_;
+private:
+    std::condition_variable cv_;
+    std::mutex m_;
+    std::queue<T> q_;
 };
-
