199d399c628029802fd943b56c730bb7afe37a80
levelup0 <leo.rybakov@icloud.com>
2021-07-12 20:34:29 +0300

    Thread_safe_vector unfinished

--- thread_safe_vector/thread_safe_vector.h
+++ thread_safe_vector/thread_safe_vector.h
@@ -1,31 +1,42 @@
 #pragma once
 
 #include <vector>
+#include <mutex>
+#include <shared_mutex>
 
 
 template <typename T>
 class ThreadSafeVector {
- public:
-  ThreadSafeVector() {
-  }
+public:
+    ThreadSafeVector() {
+    }
 
-  T operator[](size_t index) const {
-    // Your code
-    return vector_[index];
-  }
+    T operator[](size_t index) const {
+        std::shared_lock<std::shared_mutex> lock(shared_mutex_);
+        return vector_[index];
+    }
 
-  size_t Size() const {
-    // Your code
-    return vector_.size();
-  }
+    size_t Size() const {
+        std::shared_lock<std::shared_mutex> lock(shared_mutex_);
+        return vector_.size();
+    }
 
-  void PushBack(const T& value) {
-    // Your code
-    vector_.push_back(value);
-  }
+    void PushBack(const T& value) {
+        std::unique_lock<std::mutex> outer_lock(mutex_);
+        if (vector_.size() == vector_.capacity()) {
+            shared_mutex_.lock();
+            vector_.push_back(value);
+            shared_mutex_.unlock();
+        } else {
+            shared_mutex_.lock_shared();
+            vector_.push_back(value);
+            shared_mutex_.unlock_shared();
+        }
+    }
 
- private:
-  // Your code
-  std::vector<T> vector_;
+private:
+    mutable std::shared_mutex shared_mutex_;
+    mutable std::mutex mutex_;
+    std::vector<T> vector_;
 };
 
